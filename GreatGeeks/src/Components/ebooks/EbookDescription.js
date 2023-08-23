f0aa3a4f8dec0ed791dfac4f4124adae23b5ab4f
Yogita Bhargava <79744488+yogitabhargava02@users.noreply.github.com>
2023-08-23 16:50:33 +0530

    partnership content

--- GreatGeeks/src/Components/ebooks/EbookDescription.js
+++ GreatGeeks/src/Components/ebooks/EbookDescription.js
@@ -1,46 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import dummyEbooks from './dummyEbooks';
 import Navbar from '../Home/Navbar';
 import Footer from '../Home/Footer';
-import "./EbookDescription.css"
+import "./EbookDescription.css"; // Make sure this CSS file contains your styling
+
 const EbookDescription = () => {
   const { id } = useParams();
   const selectedBook = dummyEbooks.find(book => book.id.toString() === id);
-
+  const [hoverRating, setHoverRating] = useState(0);
 
   if (!selectedBook) {
     return <div>Book not found</div>;
   }
 
+  // Create an array of star icons based on the rating
+  const ratingStars = Array.from({ length: 5 }, (_, index) => (
+    <span
+      key={index}
+      className={`star ${index + 1 <= hoverRating ? 'active' : ''}`}
+      onMouseOver={() => setHoverRating(index + 1)}
+      onMouseOut={() => setHoverRating(0)}
+    >
+      ★
+    </span>
+  ));
+
   return (
-    <div>
-    <div style={{ background: 'linear-gradient(to bottom, #177692, #09080D)', minHeight: '100vh', overflowX: 'hidden' }}>
-        <Navbar />
-       
-      
-<div className='section-one'>
-<h3>{selectedBook.title}</h3>
-<img src={selectedBook.image}/>
-</div>
+    <div className='ebook-description-container'>
+      <Navbar />
+      <div className='ebook-content'>
+        <div className='section-one'>
+          <div className='left-content'>
+            <h2 className='ebook-title'>{selectedBook.title}</h2>
+            <p className='ebook-author'>{selectedBook.author}</p>
+            <p className='ebook-published-date'>{selectedBook.published_date}</p>
+            <div className='section-two'>
+             
+              <span className='divider'></span>
+              <div className='ebook-pages'>
+                <div>Pages</div>
+                <div>{selectedBook.pages}</div>
+              </div>
+            </div>
 
 
-
-
-
-
-
-
-
-      
-      <p>Author: {selectedBook.author}</p>
-      <p>Published Date: {selectedBook.published_date}</p>
-      {/* Other ebook details */}
-   
-        <Footer />
+            <div className='btns'>
+            <button className='btn'>RS.{selectedBook.price}</button>
+            <button className='btn'>FREE SAMPLE</button>
+            </div>
+          
+          </div>
+          <div className='right'>
+            <img className='books-image' src={selectedBook.image} alt={selectedBook.title} />
+            <h4>Add to Wishlist</h4>
+          </div>
+        </div>
+        <div className='aboutus-container'>
+          <h3 className='heading'>About us</h3>
+          <p className='paragraph'>{selectedBook.description}</p>
+        </div>
+        <button className='btn'>{selectedBook.type}</button>
+        <div className='ratings-reviews'>
+          <h3 className='heading'>Ratings & Reviews</h3>
+          {/* Add reviews display here */}
+        </div>
+        <div className='about-the-author'>
+          <h3 className='heading'>About the author</h3>
+          <p className='paragraph'>{selectedBook.about_the_author}</p>
+        </div>
+        <div className='rate-the-book'>
+          <h3 className='heading'>Rate this book</h3>
+          <div className='rate-this-book-content'>
+            <div className='stars'>
+              {ratingStars}
+            </div>
+            <div >
+              <button className='btn'>Write Review</button>
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
-   
   );
 };
 
--- GreatGeeks/src/Components/ebooks/dummyEbooks.js
+++ GreatGeeks/src/Components/ebooks/dummyEbooks.js
@@ -2,14 +2,15 @@ const dummyEbooks = [
   {
     id: 1,
     title: "The Adventure Begins",
+    type:"Literature",
     image:
       "https://www.junkybooks.com/administrator/bookimages/64d00f578e3629.83702370.jpg",
     author: "Jane Smith",
-    published_date: "2023-03-15",
+    published_date: "Sept 2023",
     description:
-      "Join the thrilling journey of a young hero as they set out on a quest to save their kingdom from darkness.",
+      "Join the thrilling journey of a young hero as they set out on a quest to save their kingdom from darkness.loremEiusmod ullamco adipisicing aliqua sit est ad nostrud minim incididunt consectetur tempor eiusmod culpa. Laboris velit enim elit tempor deserunt sunt reprehenderit dolore labore laborum. Aute excepteur aute aute amet esse nisi laboris laborum ullamco. Deserunt minim consequat mollit consequat laborum ipsum esse. ",
     about_the_author:
-      "Jane Smith is an avid fantasy writer with a passion for world-building and character-driven narratives.",
+      "Jane Smith is an avid fantasy writer with a passion for world-building and character-driven narratives.Excepteur sunt eiusmod labore quis amet ipsum laborum irure nisi proident reprehenderit. Qui qui enim est ea do commodo eu. Fugiat adipisicing ad sunt amet tempor elit incididunt ad quis aliquip nisi. Minim mollit minim aute incididunt id. Duis nulla quis aliqua dolore occaecat consectetur dolor excepteur mollit mollit non. Enim et duis tempor cillum nisi culpa nostrud adipisicing ullamco dolore commodo deserunt velit irure. Eu ipsum fugiat reprehenderit proident magna id voluptate duis velit tempor ut occaecat.",
     price: 9.99,
     pages: 320,
     rating: 4.5,
@@ -29,14 +30,15 @@ const dummyEbooks = [
   {
     id: 2,
     title: "Mystery at Willow Manor",
+    type:"Motivation",
     image:
       "https://www.junkybooks.com/administrator/bookimages/64e230af889140.02182710.jpg",
     author: "David Johnson",
-    published_date: "2023-06-10",
+    published_date: "Sept 2023",
     description:
-      "Unravel the secrets of Willow Manor in this gripping mystery novel filled with twists and turns.",
+      "Unravel the secrets of Willow Manor in this gripping mystery novel filled with twists and turns.loremProident voluptate aliquip deserunt anim fugiat. Eu mollit aute laboris laborum Lorem Lorem magna exercitation. Consectetur commodo sint ut ipsum. Laboris reprehenderit do nulla non nostrud aliqua dolor labore nisi ipsum voluptate commodo ad cupidatat.",
     about_the_author:
-      "David Johnson is an accomplished mystery writer, known for his intricate plots and memorable characters.",
+      "David Johnson is an accomplished mystery writer, known for his intricate plots and memorable characters.loremProident amet duis occaecat dolore non fugiat deserunt in pariatur id aliqua ipsum amet. Laborum magna est sint laboris. Adipisicing ea aliqua fugiat est eu ex adipisicing officia ea consectetur commodo culpa irure. Officia nulla Lorem veniam consequat duis fugiat ad consectetur pariatur mollit. Proident proident aliqua deserunt ipsum culpa in sint elit eiusmod nulla duis. Nulla ex fugiat velit excepteur reprehenderit velit cupidatat nisi ut nulla qui ut ex.",
     price: 12.99,
     pages: 420,
     rating: 4.8,
@@ -57,14 +59,15 @@ const dummyEbooks = [
   {
     id: 3,
     title: "Science Unleashed",
+    type:"Fiction and Literature",
     image:
       "https://www.junkybooks.com/administrator/bookimages/6483a6a3d6e865.99866211.jpg",
     author: "Michael Anderson",
-    published_date: "2023-01-22",
+    published_date: "Sept 2023",
     description:
-      "Embark on a journey through the fascinating world of scientific discoveries that changed the course of history.",
+      "Embark on a journey through the fascinating world of scientific discoveries that changed the course of history.loremIrure pariatur elit elit commodo laborum Lorem cillum ex. Anim amet cupidatat officia qui ut incididunt minim voluptate ad officia laboris nisi velit. Incididunt eu ad nostrud et excepteur anim exercitation aliqua elit. Ullamco irure anim anim cillum sint nulla tempor cupidatat mollit nisi aliqua consequat aute. Quis amet reprehenderit in minim eu. Do ullamco ad est ullamco est nulla. Mollit sunt aute reprehenderit occaecat labore adipisicing in cillum nisi pariatur ipsum ad.",
     about_the_author:
-      "Michael Anderson is a renowned scientist and educator, passionate about making complex concepts accessible to everyone.",
+      "Michael Anderson is a renowned scientist and educator, passionate about making complex concepts accessible to everyone.Labore aliquip aliquip ad ut consequat aliquip duis non. Anim amet irure quis cillum tempor elit fugiat eiusmod adipisicing irure nulla anim. Voluptate deserunt exercitation labore nostrud non deserunt pariatur nulla ad deserunt.",
     price: 14.99,
     pages: 280,
     rating: 4.6,
